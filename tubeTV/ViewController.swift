01f7ff428025a631d180a44b04582c2ef4819bfc
しゅ いりん <vvv236@shiyumato-macbook-pro.local>
2018-12-19 22:54:46 +0900

    Initial Commit

---
+++ tubeTV/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tubeTV
+//
+//  Created by しゅ いりん on 2018/12/19.
+//  Copyright © 2018年 ChuWeiLun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
